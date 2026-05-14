eb9be43bffdcd0741654e5cf195d69127e3246b6
saadhiq <hassaan.edu@gmail.com>
2026-05-14 10:32:01 +0530

    port url updated

--- app.py
+++ app.py
@@ -56,6 +56,6 @@ def chat():
     return jsonify({"response": last_message.content})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
     
